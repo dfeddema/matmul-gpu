3dd35dc45ffbacc4948d2c91f8949c4d860ba62d
Selbi <43946617+heyselbi@users.noreply.github.com>
2020-07-28 12:11:29 -0400

    Update matmul.py

--- matmul.py
+++ matmul.py
@@ -16,7 +16,7 @@ else:
     device_name = "/cpu:0"
 
 with tf.device(device_name):
-    random_matrix = tf.random_uniform(shape=shape, minval=0, maxval=1)
+    random_matrix = tf.random.uniform(shape=shape, minval=0, maxval=1)
     dot_operation = tf.matmul(random_matrix, tf.transpose(random_matrix))
     sum_operation = tf.reduce_sum(dot_operation)
 
